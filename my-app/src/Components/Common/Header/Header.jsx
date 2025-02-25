abb012fe4f6066a2a7c76246efc0fe7f0062a567
Chanuka999 <chanukaranditha99@gmail.com>
2025-02-25 12:57:26 +0530

    add

---
+++ my-app/src/Components/Common/Header/Header.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { nav } from "../../data/Data";
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <div className="container flex">
+          <div className="logo">
+            <img src="./images/logo.avif" alt="" />
+          </div>
+          <div className="nav">
+            <ul className="flex">
+              {nav.map((list, index) => (
+                <li key={index}>
+                  <Link to={list.path}>{list.text}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
