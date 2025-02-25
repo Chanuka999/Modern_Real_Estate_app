8753a09a7b689770e5fdf1dcc70d77fed6241a2a
Chanuka999 <chanukaranditha99@gmail.com>
2025-02-25 12:57:07 +0530

    added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Header from "./Components/Common/Header/Header";
+//import Pages from "./Components/page/Pages";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
